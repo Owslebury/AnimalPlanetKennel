4cc5b160cae77a74f2e980f857504a9cbe28997d
Owslebury <johnnyturnbull8788@gmail.com>
2023-03-08 12:32:22 +0000

    changed cat methods to generic animal

--- src/Animal.java
+++ src/Animal.java
@@ -1,5 +1,11 @@
+import java.util.ArrayList;
+
 public class Animal {
-    Owner owner;
+    private ArrayList<Owner> originalOwners;
+    private boolean canShare;
+    private String catName;
+    private String favFood;
+    private int foodPerDay;
     protected Boolean likesBones;
     protected Boolean sharesRuns;
 }
